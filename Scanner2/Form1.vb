c2e2942c447c9caeb696fd08e5e6fc87dffba6b6
Демчина Орест <ODemchyna@hlibprom.com.ua>
2021-04-21 15:51:37 +0300

    Add project files.

---
+++ Scanner2/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button_IN_Click(sender As Object, e As EventArgs) Handles Button_IN.Click
+        Dim dialog = New FolderBrowserDialog()
+        dialog.SelectedPath = Application.StartupPath
+        If DialogResult.OK = dialog.ShowDialog() Then
+            TextBox_IN.Text = dialog.SelectedPath
+        End If
+    End Sub
+End Class
