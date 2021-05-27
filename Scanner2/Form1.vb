35e70e2de1fad659a7e3ab04f77cba3e63876bc8
Orest Demchyna <7dorko@gmail.com>
2021-05-27 13:29:01 +0300

    Delete Scanner2 directory

--- Scanner2/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button_IN_Click(sender As Object, e As EventArgs) Handles Button_IN.Click
-        Dim dialog = New FolderBrowserDialog()
-        dialog.SelectedPath = Application.StartupPath
-        If DialogResult.OK = dialog.ShowDialog() Then
-            TextBox_IN.Text = dialog.SelectedPath
-        End If
-    End Sub
-End Class
